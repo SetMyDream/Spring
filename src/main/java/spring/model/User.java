a895ed71cca6fe8de71b712d69799af42763e994
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-10-19 17:05:31 +0300

    solution 1

---
+++ src/main/java/spring/model/User.java
@@ -0,0 +1,21 @@
+package spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private byte[] salt;
+}
